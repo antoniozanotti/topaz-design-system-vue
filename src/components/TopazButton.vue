8ce9127d342c8153170b73ccdef100a796f9018c
Antônio Zanotti <antoniorzanotti@gmail.com>
2023-11-06 13:35:36 -0300

    feat: add dark mode to button component

--- src/components/TopazButton.vue
+++ src/components/TopazButton.vue
@@ -23,6 +23,24 @@ const iconName = computed(() =>
   props.iconName ? heroIcons[props.iconName] : ""
 );
 
+const variantStyleMode = computed(() => {
+  if (document.documentElement.classList.contains('dark')){
+    if(props.variant == "primary"){
+      return "secondary";
+    }
+    if(props.variant == "secondary"){
+      return "primary";
+    }
+    if(props.variant == "black"){
+      return "white";
+    }
+    if(props.variant == "white"){
+      return "black";
+    }
+  }
+  return props.variant;
+});
+
 /* button style */
 const classes = computed(() => ({
   // default
@@ -32,11 +50,14 @@ const classes = computed(() => ({
   "focus:outline focus:outline-offset-2px focus:outline-2px": true,
 
   // size
-  "text-sm   min-h-42px min-w-42px lg:text-xs   lg:min-h-34px lg:min-w-34px":
+  // size -> small
+  "text-sm min-h-42px min-w-42px lg:text-xs lg:min-h-34px lg:min-w-34px":
     props.size == "small",
-  "text-base min-h-60px min-w-60px lg:text-sm   lg:min-h-48px lg:min-w-48px":
+  // size -> base
+  "text-base min-h-60px min-w-60px lg:text-sm lg:min-h-48px lg:min-w-48px":
     props.size == "medium",
-  "text-lg   min-h-72px min-w-72px lg:text-base lg:min-h-58px lg:min-w-58px":
+  // size -> large
+  "text-lg min-h-72px min-w-72px lg:text-base lg:min-h-58px lg:min-w-58px":
     props.size == "large",
 
   // padding only with text
@@ -46,44 +67,44 @@ const classes = computed(() => ({
 
   // fill
   // fill -> accent
-  "bg-accent hover:bg-accent-600 active:bg-accent-700 text-light focus:outline-accent dark:bg-white":
-    props.style == "fill" && props.variant == "accent",
+  "bg-accent hover:bg-accent-600 active:bg-accent-700 text-light dark:text-dark focus:outline-accent":
+    props.style == "fill" && variantStyleMode.value == "accent",
   // fill -> primary
   "bg-primary hover:bg-primary-600 active:bg-primary-700 text-light focus:outline-primary":
-    props.style == "fill" && props.variant == "primary",
+    props.style == "fill" && variantStyleMode.value == "primary",
   // fill -> secondary
   "bg-secondary hover:bg-secondary-600 active:bg-secondary-700 text-primary focus:outline-secondary":
-    props.style == "fill" && props.variant == "secondary",
+    props.style == "fill" && variantStyleMode.value == "secondary",
   // fill -> negative
-  "bg-negative hover:bg-negative-600 active:bg-negative-700 text-light focus:outline-negative":
-    props.style == "fill" && props.variant == "negative",
+  "bg-negative hover:bg-negative-600 active:bg-negative-700 text-light dark:text-dark focus:outline-negative":
+    props.style == "fill" && variantStyleMode.value == "negative",
   // fill -> black
   "bg-dark hover:bg-dark-700 active:bg-dark-600 text-light focus:outline-dark":
-    props.style == "fill" && props.variant == "black",
+    props.style == "fill" && variantStyleMode.value == "black",
   // fill -> white
   "bg-light hover:bg-light-600 active:bg-light-700 text-dark focus:outline-light":
-    props.style == "fill" && props.variant == "white",
+    props.style == "fill" && variantStyleMode.value == "white",
 
   // outline
   "outline outline-1px": props.style == "outline",
   // outline -> accent
   "outline-accent hover:outline-accent-600 active:outline-accent-700 active: text-accent":
-    props.style == "outline" && props.variant == "accent",
+    props.style == "outline" && variantStyleMode.value == "accent",
   // outline -> primary
-  "outline-primary  hover:outline-primary-600 active:outline-primary-700 text-primary":
-    props.style == "outline" && props.variant == "primary",
+  "outline-primary hover:outline-primary-600 active:outline-primary-700 text-primary":
+    props.style == "outline" && variantStyleMode.value == "primary",
   // outline -> secondary
-  "outline-secondary hover:outline-secondary-600 active:outline-secondary-700  text-primary":
-    props.style == "outline" && props.variant == "secondary",
+  "outline-secondary hover:outline-secondary-600 active:outline-secondary-700 text-primary":
+    props.style == "outline" && variantStyleMode.value == "secondary",
   // outline -> negative
-  "outline-negative hover:outline-negative-600 active:outline-negative-700  text-negative":
-    props.style == "outline" && props.variant == "negative",
+  "outline-negative hover:outline-negative-600 active:outline-negative-700 text-negative":
+    props.style == "outline" && variantStyleMode.value == "negative",
   // outline -> black
-  "outline-dark hover:outline-dark-700 active:outline-dark-600  text-dark":
-    props.style == "outline" && props.variant == "black",
+  "outline-dark hover:outline-dark-700 active:outline-dark-600 text-dark":
+    props.style == "outline" && variantStyleMode.value == "black",
   // outline -> white
-  "outline-light hover:outline-light-600 active:outline-light-700  text-light":
-    props.style == "outline" && props.variant == "white",
+  "outline-light hover:outline-light-600 active:outline-light-700 text-light":
+    props.style == "outline" && variantStyleMode.value == "white",
 
   // is disabled
   "opacity-50 pointer-events-none": props.isDisabled,
