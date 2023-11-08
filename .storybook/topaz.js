82b5159595f3c466cd0a45fc67c204f88c37eb0b
Antônio Zanotti <antoniorzanotti@gmail.com>
2023-11-07 22:23:29 -0300

    feat: updated project name

--- .storybook/topaz.js
+++ .storybook/topaz.js
@@ -1,10 +1,10 @@
 import { create } from '@storybook/theming/create';
-import topazBrand from './topaz-design-system-brand.svg?url'
+import topazBrand from './topaz-brand.svg?url'
 
 export default create({
   base: 'light',
   brandTitle: 'Topaz Design System Vue',
-  brandUrl: 'https://topaz-design-system-vue.netlify.app',
+  brandUrl: 'https://topaz-vue.netlify.app',
   brandImage: topazBrand,
   brandTarget: '_self',
 });
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# topaz-design-system-vue
+# topaz-vue
 
 This template should help get you started developing with Vue 3 in Vite.
 
